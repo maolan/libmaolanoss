9b0da3528210bca18d0ea5a5fc360021a9786f9b
Goran Mekić <meka@tilda.center>
2023-07-18 09:32:30 +0200

    Use core library dynamic loader

--- tests/load.cpp
+++ tests/load.cpp
@@ -1,8 +1,3 @@
-#include <dlfcn.h>
-#include <map>
-#include <string>
-#include <maolan/audio/hw.hpp>
-#include <maolan/midi/hw.hpp>
 #include <maolan/dynamichw.hpp>
 
 
@@ -11,18 +6,11 @@ using namespace maolan;
 
 int main()
 {
-  void *handle;
-  DynamicHW oss;
+  DynamicHW oss{"./libmaolanoss.so"};
 
-  handle = dlopen("./libmaolanoss.so", RTLD_LAZY);
-  oss.audio["out"] = (audio::audio_t)dlsym(handle, "createAudioOut");
-  oss.audio["in"] = (audio::audio_t)dlsym(handle, "createAudioIn");
-  oss.midi["out"] = (midi::midi_t)dlsym(handle, "createMidiOut");
-  oss.midi["in"] = (midi::midi_t)dlsym(handle, "createMidiIn");
-
-  oss.audio["out"]("OssAudioOut", "/dev/dsp", 4);
-  oss.audio["in"]("OssAudioOut", "/dev/dsp", 4);
-  oss.midi["out"]("OssMidiOut", "/dev/dsp");
-  oss.midi["in"]("OssMidiIn", "/dev/dsp");
+  oss.audio.out("OssAudioOut", "/dev/dsp", 4);
+  oss.audio.in("OssAudioOut", "/dev/dsp", 4);
+  oss.midi.out("OssMidiOut", "/dev/dsp");
+  oss.midi.in("OssMidiIn", "/dev/dsp");
   return 0;
 }
